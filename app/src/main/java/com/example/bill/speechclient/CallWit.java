8304455e246b5fdce92a99abaed81ca7edbc5bb7
gvol1222 <gsystemin@gmail.com>
2017-10-24 00:44:57 +0300

    Changed Url Encoding

--- app/src/main/java/com/example/bill/speechclient/CallWit.java
+++ app/src/main/java/com/example/bill/speechclient/CallWit.java
@@ -4,6 +4,7 @@ package com.example.bill.speechclient;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.util.Xml;
 
 
 import java.io.BufferedInputStream;
@@ -11,9 +12,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by bill on 10/23/17.
@@ -59,11 +62,19 @@ public class CallWit extends AsyncTask {
 
         String urlString = objects[0].toString();
         String message  = objects[1].toString();
+
+        String messageencoded  = null;
+        try {
+            messageencoded = URLEncoder.encode(message,"UTF-8");
+            Log.d("BILLOG",messageencoded);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         String result = "";
         BufferedReader in =null;
         HttpURLConnection connection;
         try {
-            URL url = new URL(urlString+message);
+            URL url = new URL(urlString+messageencoded);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept","application/json");
