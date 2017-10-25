c573df7b9891354ae2c2023b7c3892676e280a8f
doc1991 <bmarnt@gmail.com>
2017-10-25 21:38:18 +0300

    test

---
+++ app/src/main/java/com/example/bill/speechclient/LaunchApp.java
@@ -0,0 +1,50 @@
+package com.example.bill.speechclient;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by bill on 10/25/17.
+ */
+
+public abstract class LaunchApp {
+
+    private Intent IntentApp;
+    private Context context;
+
+    public LaunchApp( String action,Context context) {
+        this.context = context;
+        IntentApp = new Intent(action);
+
+    }
+
+    public LaunchApp(Context context) {
+        this.context = context;
+        IntentApp = new Intent();
+
+    }
+
+
+
+    public void AddFlag(int flag){
+        IntentApp.setFlags(flag);
+    }
+    public void AddExtra(String name,String extra){
+        IntentApp.putExtra(name,extra);
+    }
+
+
+
+    public void setData(Uri uri){
+        IntentApp.setData(uri);
+    }
+    public void SetData(String PackageName){
+            IntentApp.setPackage(PackageName);
+    }
+
+    public void TriggerIntent(){
+        context.startActivity(IntentApp);
+    }
+
+}
