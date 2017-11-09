173f6d5f8508143d35ac541d763613db65fd9c80
doc1991 <bmarnt@gmail.com>
2017-11-09 17:53:57 +0200

    test

--- app/src/main/java/Applications/CallTel.java
+++ app/src/main/java/Applications/CallTel.java
@@ -1,37 +1,35 @@
 package Applications;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+
+import Utils.ContactUtils;
 
 /**
  * Created by bill on 10/26/17.
  */
 
-public class CallTel extends LaunchApp {
+public class CallTel {
 
-    public CallTel(String action, Context context) {
-        super(action, context);
+
+    public static void TriggerCall(@NonNull String data, @NonNull Context context) {
+        final ArrayList<String> tel = ContactUtils.ContactNumber(data, context);
+        if (tel.size() == 1) {
+            newCall(Constatns.action, Constatns.flag, tel.get(0), context);
+        }
+
     }
 
-
-    @Override
-    public void AddFlag(int flag) {
-        super.AddFlag(flag);
-    }
-
-    @Override
-    public void setData(Uri uri) {
-        super.setData(uri);
-    }
-
-    @Override
-    public void SetData(String PackageName) {
-        super.SetData(PackageName);
-    }
-
-    @Override
-    public void TriggerIntent() {
-        super.TriggerIntent();
+    private static void newCall(@NonNull  final String action, @NonNull final int flag, @NonNull String number, @NonNull Context context) {
+        final Intent intent = new Intent(action);
+        intent.setFlags(flag);
+        intent.setData(Uri.parse("tel:" + number));
+        if (intent.resolveActivity(context.getPackageManager()) != null)
+            context.startActivity(intent);
     }
 
 
