b25c23fe1f104025f19bc96f4f3f931847d69acb
doc1991 <bmarnt@gmail.com>
2017-11-09 18:04:16 +0200

    test

--- app/src/main/java/Applications/CallTel.java
+++ app/src/main/java/Applications/CallTel.java
@@ -3,7 +3,8 @@ package Applications;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+
+import com.example.bill.speechclient.R;
 
 import java.util.ArrayList;
 
@@ -16,15 +17,21 @@ import Utils.ContactUtils;
 public class CallTel {
 
 
-    public static void TriggerCall(@NonNull String data, @NonNull Context context) {
+    public static String TriggerCall(final String data, Context context) {
         final ArrayList<String> tel = ContactUtils.ContactNumber(data, context);
+
         if (tel.size() == 1) {
             newCall(Constatns.action, Constatns.flag, tel.get(0), context);
-        }
+            return context.getResources().getString(R.string.make_call_acces_message);
+        } else if (tel.size() < 0 || tel == null) {
+            return context.getResources().getString(R.string.make_call_error_message);
+        } else {
 
+        }
+        return "";
     }
 
-    private static void newCall(@NonNull  final String action, @NonNull final int flag, @NonNull String number, @NonNull Context context) {
+    private static void newCall(final String action, final int flag, String number, Context context) {
         final Intent intent = new Intent(action);
         intent.setFlags(flag);
         intent.setData(Uri.parse("tel:" + number));
