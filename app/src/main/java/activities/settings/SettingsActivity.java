284ae3e0b222731292f899273dd1bdc19d09157f
doc1991 <bmarnt@gmail.com>
2017-12-08 15:45:22 +0200

    test

--- app/src/main/java/activities/settings/SettingsActivity.java
+++ app/src/main/java/activities/settings/SettingsActivity.java
@@ -3,6 +3,7 @@ package activities.settings;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.bill.Activities.R;
 
@@ -13,11 +14,24 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
         getFragmentManager().beginTransaction()
                 .replace(R.id.pref_container, new SettingsFragment())
                 .commit();
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
